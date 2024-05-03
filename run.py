95109a7e32dedd79f479f3d4515b3bc34be711de
Luthfi Fadle Rynaldy <luthfifadle9d@gmail.com>
2024-05-04 00:22:42 +0700

    ngrok

--- run.py
+++ run.py
@@ -1,6 +1,19 @@
-#!/usr/bin/env python3
 
 from facefusion import core
+
+import getpass
+
+from pyngrok import ngrok, conf
+
+print("Enter your authtoken, which can be copied from https://dashboard.ngrok.com/auth")
+conf.get_default().auth_token = "2fsCEZ7yGRcBnSsuX4iRRintOJ7_7254e3HQMifzNH8jFm2qQ"
+
+# Open a TCP ngrok tunnel to the SSH server
+connection_string = ngrok.connect("7860", "tcp").public_url
+
+public_url = ngrok.connect(7860).public_url
+print(" * ngrok tunnel \"{}\" -> \"http://127.0.0.1:{}/\"".format(public_url, 7860))
+
 
 if __name__ == '__main__':
 	core.cli()
